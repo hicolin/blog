bcb9af2f0eac2dc6824a0e660c02247722243d1b
Colin <811687790@qq.com>
2019-08-09 16:17:41 +0800

    百度统计

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,6 +26,17 @@ AppAsset::register($this);
         <?= $this->blocks['header']; ?>
     <?php endif; ?>
 
+    <!--百度统计-->
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?d6dbaf3b45a98c6a0d3ce39ab6325dda";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+
 </head>
 
 <body>
